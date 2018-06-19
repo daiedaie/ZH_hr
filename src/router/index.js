3940f7efa82770583b865f42a350a54f80bae774
peng0716 <284393960@qq.com>
2018-06-19 09:27:16 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/views/Login'
+import Home from '@/views/Home'
+// import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    }
+  ]
+})
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    home
+  </div>
+</template>
+<script>
+export default {
+  name: "home",
+  props: {},
+  data () {
+    return {};
+  },
+  watch: {},
+  methods: {},
+  mounted () {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
